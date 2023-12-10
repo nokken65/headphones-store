026b55a0539ac69a269e66d8da2c6c11da282f93
nokken65 <unknown65182@gmail.com>
2023-12-10 03:45:59 +0300

    refactor: change app root id

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('app-root') as HTMLElement)
 
 root.render(
   // <React.StrictMode>

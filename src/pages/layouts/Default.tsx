4ab724d91b5896cfdec56ecf3a1d035dd5002265
nokken65 <unknown65182@gmail.com>
2023-12-10 03:49:42 +0300

    feat: add Default layout component

---
+++ src/pages/layouts/Default.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Footer } from '@/widgets/Footer'
+import { Header } from '@/widgets/Header/ui'
+
+type DefaultLayoutProps = React.PropsWithChildren
+
+const _DefaultLayout = ({ children }: DefaultLayoutProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+export const DefaultLayout = React.memo(_DefaultLayout)

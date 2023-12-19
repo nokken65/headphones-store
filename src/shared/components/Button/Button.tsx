2a0ff36e7a8f7376b23bfdc0601c43ad8bf99c39
nokken65 <unknown65182@gmail.com>
2023-12-19 04:48:18 +0300

    refactor: Button to pure styled component

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,15 +1,84 @@
-import React from 'react'
+import { css, styled } from 'styled-components'
 
-import * as S from './Button.styled'
+type ButtonProps = {
+  $variant?: 'filled' | 'outline' | 'subtle'
+  $iconId?: string
+}
 
-type ButtonProps = React.ComponentPropsWithoutRef<'button'>
+const _Button = styled.button.attrs<ButtonProps>(props => ({
+  ...props,
+  type: props.type ?? 'button',
+  $variant: props.$variant ?? 'filled',
+}))`
+  appearance: none;
+  padding: 1rem 1.5rem;
+  border-radius: 1rem;
+  cursor: pointer;
 
-const _Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...rest }, ref) => {
-  return (
-    <S.Button {...rest} ref={ref}>
-      {children}
-    </S.Button>
-  )
-})
+  font-weight: 500;
 
-export const Button = React.memo(_Button)
+  &:focus-visible {
+    outline-offset: 0.2rem;
+    outline: 2px solid orange;
+  }
+
+  transition:
+    transform 0.1s ease-in-out,
+    color 0.1s ease-in-out,
+    border 0.1s ease-in-out,
+    background-color 0.1s ease-in-out;
+
+  ${({ theme, $variant }) => {
+    switch ($variant) {
+      case 'filled':
+        return css`
+          border: 2px solid ${theme.colors.text};
+          background-color: ${theme.colors.text};
+          color: ${theme.colors.textInvert};
+
+          &:hover {
+            background-color: transparent;
+            color: ${theme.colors.text};
+          }
+
+          &:active {
+            transform: scale(0.98);
+          }
+        `
+      case 'outline':
+        return css`
+          border: 2px solid ${theme.colors.text};
+          background-color: transparent;
+          color: ${theme.colors.text};
+
+          &:hover {
+            background-color: ${theme.colors.text};
+            color: ${theme.colors.textInvert};
+          }
+
+          &:active {
+            transform: scale(0.98);
+          }
+        `
+      case 'subtle':
+        return css`
+          border: 0;
+          background-color: transparent;
+          color: ${theme.colors.text};
+
+          &:hover {
+            background-color: ${theme.colors.text};
+            color: ${theme.colors.textInvert};
+          }
+
+          &:active {
+            transform: scale(0.98);
+          }
+        `
+      default:
+        return ''
+    }
+  }}
+`
+
+export const Button = _Button

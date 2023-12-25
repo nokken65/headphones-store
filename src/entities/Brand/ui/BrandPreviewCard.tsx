b58c97b8a33dfc050a6dc5741bc974874d1771fb
nokken65 <unknown65182@gmail.com>
2023-12-25 03:08:43 +0300

    refactor: set Brand Card initial size

--- src/entities/Brand/ui/BrandPreviewCard.tsx
+++ src/entities/Brand/ui/BrandPreviewCard.tsx
@@ -16,7 +16,13 @@ const BrandPreviewCard = ({ name, src }: BrandPreviewCardProps) => {
         <source srcSet={`${src.avif} 1x`} type={'image/avif'} />
         <source srcSet={`${src.webp} 1x`} type={'image/webp'} />
         <source srcSet={`${src.png} 1x`} type={'image/png'} />
-        <S.Image alt={`${name} logo image`} loading={'lazy'} src={src.png} />
+        <S.Image
+          alt={`${name} logo image`}
+          height={37}
+          loading={'lazy'}
+          src={src.png}
+          width={250}
+        />
       </picture>
     </S.Card>
   )

c093258cb3e53b64bdb009b3bec2b0aced91e248
nokken65 <unknown65182@gmail.com>
2023-12-27 00:17:37 +0300

    chore: cleanup deps

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
           'react-vendor': ['react', 'react-dom'],
           'nanostores-vendor': ['nanostores', '@nanostores/persistent', '@nanostores/react'],
           'styled-components-vendor': ['styled-components'],
-          'framer-motion-vendor': ['framer-motion'],
           'utils-vendor': ['ramda', '@uidotdev/usehooks'],
         },
       },

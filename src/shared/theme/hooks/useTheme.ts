c4cae9555ba4fe6bddbf6c889ed38e92e275f749
nokken65 <unknown65182@gmail.com>
2024-01-08 03:11:29 +0300

    refactor: useInsertionEffect when attach css vars

--- src/shared/theme/hooks/useTheme.ts
+++ src/shared/theme/hooks/useTheme.ts
@@ -8,47 +8,19 @@ import { common } from '../themes/common'
 import { getTheme } from '../utils/getTheme'
 import { objToArray } from '../utils/objToArray'
 
-const COLOR_SCHEME_ATTRIBUTE_NAME = 'data-color-scheme' as const
-
 const useTheme = (colorScheme: ColorScheme) => {
-  React.useEffect(() => {
+  React.useInsertionEffect(() => {
     setCSSStyleSheet({
       vars: objToArray(common),
       index: 0,
     })
 
-    const mutationCallback = (records: MutationRecord[]) => {
-      const record = records.find(record => record.attributeName === COLOR_SCHEME_ATTRIBUTE_NAME)
-
-      if (!record) {
-        return
-      }
-
-      const scheme = (record.target as HTMLElement).getAttribute(
-        record.attributeName ?? ''
-      ) as Exclude<ColorScheme, 'auto'> | null
-
-      if (scheme === null) {
-        return
-      }
-
-      setCSSStyleSheet({
-        vars: objToArray(getTheme(scheme)),
-        index: 1,
-      })
-    }
-
-    const observer = new MutationObserver(mutationCallback)
-
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: [COLOR_SCHEME_ATTRIBUTE_NAME],
+    setCSSStyleSheet({
+      vars: objToArray(getTheme(colorScheme)),
+      index: 1,
     })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
-  React.useEffect(() => {
-    document.documentElement.setAttribute(COLOR_SCHEME_ATTRIBUTE_NAME, colorScheme)
+    document.documentElement.setAttribute('data-color-scheme', colorScheme)
   }, [colorScheme])
 }
 
